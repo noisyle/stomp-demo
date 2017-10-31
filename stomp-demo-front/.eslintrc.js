b797702af7076cd1600c34170f8537c402dc40f9
York Wang <anyone.1984@gmail.com>
2017-10-31 17:59:54 +0800

    add docker files

--- stomp-demo-front/.eslintrc.js
+++ stomp-demo-front/.eslintrc.js
@@ -8,6 +8,10 @@ module.exports = {
   },
   env: {
     browser: true,
+    "commonjs": true,
+    "amd": true,
+    "es6": true,
+    "jquery": true
   },
   // https://github.com/standard/standard/blob/master/docs/RULES-en.md
   extends: 'standard',
@@ -22,6 +26,18 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    "indent": [
+      "warn",
+      2
+    ],
+    //关闭额外的分号检查
+    //0:关闭，1:警告，2:异常
+    "semi": 0,
+    //字符串必须使用单引号
+    "quotes": [
+      "error",
+      "single"
+    ]
   }
 }
